4a5f4d53a6350057ce6e14259aa1c4a385254511
space doge <99536044+d0g3bl2@users.noreply.github.com>
2023-02-14 10:29:40 +0000

    add change

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React CAN YOU SEE THIS</h1>
+      <h1>Vite + React CAN YOU SEE THIS STAR moon Sun</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
