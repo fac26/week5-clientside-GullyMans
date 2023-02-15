7fb840e7fa4d6fa643d4684c6249a1f948677aa3
ighellache <78818760+ighellache@users.noreply.github.com>
2023-02-15 17:13:56 +0000

    bananajoe no longer in css

--- src/characters.jsx
+++ src/characters.jsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react';
 // import './App.css';
-import Battle from './battleoutcome';
+//import Battle from './battleoutcome';
 
 export default function Characters({ playerHealth, cpuHealth }) {
   return (
     <section id="characters">
       <div id="p1flex">
-        <div id="P1">{/* player one fight display */}</div>
+        <img  src="/bananajoe.webp" id="P1"/>
         <p>{playerHealth}</p>
       </div>
       <div id="cpuflex">
